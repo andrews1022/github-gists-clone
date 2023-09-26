a7cda55d03c0ccc0432a2527a520d70d90a0688f
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-26 16:38:58 -0700

    configured next-auth drizzle adapter
    - installed @auth/drizzle-adapter package
    - created drizzle config file to easily re-use db instance
    - updated next-auth options to use drizzle adapter, passing in exported db

--- src/next-auth/options.ts
+++ src/next-auth/options.ts
@@ -1,7 +1,11 @@
 import GitHubProvider from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/drizzle/config";
+
 import type { AuthOptions } from "next-auth";
 
 const options: AuthOptions = {
+  adapter: DrizzleAdapter(db),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
