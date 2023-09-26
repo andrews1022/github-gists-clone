d92f07880d2695aada9ded9c63cc124f1e64eb8e
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-26 16:31:17 -0700

    updated nextauth options to use github oauth provider

--- src/next-auth/options.ts
+++ src/next-auth/options.ts
@@ -1,7 +1,13 @@
+import GitHubProvider from "next-auth/providers/github";
 import type { AuthOptions } from "next-auth";
 
 const options: AuthOptions = {
-  providers: []
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET_ID!
+    })
+  ]
 };
 
 export { options };
