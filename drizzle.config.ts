469101d3c825aa70adf5987b00f9c3baf7016d49
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-26 17:54:28 -0700

    migrated and pushed tables to supabase db
    - updated gitignore with files and folders as per drizzle repo
    - installed dotenv pckg to use in drizzle.config
    - added generate and push scripts in package.json
    - changed target in tsconfig to allow drizzle config to work
    - ran initial db migration with drizzle
    - added table code for schema from next auth drizzle adapter page in the docs

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,8 @@
+import dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
+
+// this is needed for pushing
+dotenv.config({ path: ".env.local" });
 
 const config: Config = {
   schema: "./src/drizzle/schema.ts",
