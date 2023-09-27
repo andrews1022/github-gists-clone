317754589691442b56fc24a82d6c225437d9c1e4
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-27 14:01:45 -0700

    temporary fix: removed middleware and added session check on gists page

--- src/app/gists/page.tsx
+++ src/app/gists/page.tsx
@@ -1,14 +1,19 @@
+import { options } from "@/next-auth/options";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-import { options } from "@/next-auth/options";
+const GistsPage = async () => {
+  const session = await getServerSession(options);
 
-const GistsPage = () => {
-  return (
-    <div>
-      <h1>GistsPage</h1>
-    </div>
-  );
+  if (!session) {
+    redirect("/sign-in");
+  } else {
+    return (
+      <div>
+        <h1>GistsPage</h1>
+      </div>
+    );
+  }
 };
 
 export default GistsPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-const HomePage = () => {
+import { getServerSession } from "next-auth";
+import { options } from "@/next-auth/options";
+
+const HomePage = async () => {
+  const session = await getServerSession(options);
+
   return (
     <div>
       <h1>HomePage</h1>
+
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 };
