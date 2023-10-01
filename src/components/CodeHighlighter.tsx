b621da4366234991c1d8635131d6ce28cca72d39
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-30 18:50:49 -0700

    finally solved added lines up to 10 in CodeHighlighter
    - HAHA I CAN FINALLY MOVE AWAY FROM THIS STUPID F*CKING COMPONENT

--- src/components/CodeHighlighter.tsx
+++ src/components/CodeHighlighter.tsx
@@ -10,47 +10,121 @@ type CodeHighlighterProps = {
 
 const MAX_NUMBER_OF_LINES = 10;
 
+// (
+//   <div key={index} {...getLineProps({ line })}>
+//     <span>{index < 9 ? `0${index + 1}` : index + 1}</span>
+
+//     {/* {JSON.stringify(line, null, 2)} */}
+//   </div>
+// )
+
 const CodeHighlighter = ({ code, fileNameAndExtension }: CodeHighlighterProps) => {
   return (
     <Highlight theme={themes.oneLight} code={code} language="tsx">
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre style={style} className="p-4 overflow-x-scroll rounded-lg">
-          {tokens.length < MAX_NUMBER_OF_LINES && <p>Add extra lines to get up to 10</p>}
+          {/* CODE SNIPPET HAS FEWER THAN 10 LINES */}
+          {tokens.length < MAX_NUMBER_OF_LINES &&
+            tokens.map((line, index) => (
+              <div key={index} {...getLineProps({ line })}>
+                <span>{index + 1}</span>
 
-          {tokens.length > MAX_NUMBER_OF_LINES && (
-            <p>Remove {tokens.length - MAX_NUMBER_OF_LINES} lines</p>
-          )}
+                {line.map((token, idx) => (
+                  <span
+                    key={idx}
+                    {...getTokenProps({ token })}
+                    className={idx === 0 ? "pl-4" : ""}
+                  />
+                ))}
+              </div>
+            ))}
 
-          {tokens.length === MAX_NUMBER_OF_LINES && <p>Perfect!</p>}
+          {/*
+            create x number of divs, where x is equal to the result of MAX_NUMBER_OF_LINES - tokens.length
+            then map over that array and create a div for each item
+            display the index of the item inside a span tag inside the div
+            the values display in the span tags should pick up where the line numbers left off
+            for example, if the code snippet had 8 lines, the last 2 divs should display 9 and 10
+          */}
+          {tokens.length < MAX_NUMBER_OF_LINES &&
+            Array.from({ length: MAX_NUMBER_OF_LINES - tokens.length }, (_, index) => (
+              <div key={index}>
+                <span>{tokens.length + index + 1}</span>
+              </div>
+            ))}
 
-          {/* {tokens.slice(0, MAX_NUMBER_OF_LINES).map((line, i) => (
-            <div key={i} {...getLineProps({ line })} className="hi">
-              <span className="1">{i + 1}</span>
-              {line.map((token, index) => (
-                <span
-                  key={index}
-                  {...getTokenProps({ token })}
-                  className={index === 0 ? "pl-4" : ""}
-                />
-              ))}
+          {/* {Array.from({ length: MAX_NUMBER_OF_LINES - tokens.length }, (_, index) => (
+            // <pre>hi</pre>
+            <div key={index}>
+
+
+              <span>{MAX_NUMBER_OF_LINES - index}</span>
             </div>
           ))} */}
 
-          {/* {tokens.length < MAX_NUMBER_OF_LINES ? (
-            <>
-              {Array.from({ length: MAX_NUMBER_OF_LINES - tokens.length }).map((_, index) => (
-                <div key={index} className="hi">
-                  <span className="1">{index}</span>
-                </div>
-              ))}
-            </>
-          ) : null} */}
-
-          {/* {tokens.length < MAX_NUMBER_OF_LINES ? (
-            <div>
-              <p>Insert {MAX_NUMBER_OF_LINES - tokens.length} lines</p>
+          {/* {Array.from({ length: MAX_NUMBER_OF_LINES - tokens.length }).map((_, index) => (
+            <div key={index}>
+              <span >{index}</span>
             </div>
-          ) : null} */}
+          ))} */}
+
+          {/* {tokens.length < MAX_NUMBER_OF_LINES &&
+            [...tokens, ...Array(MAX_NUMBER_OF_LINES - tokens.length)].map(
+              (line: Token[], index) => {
+                // Fix the error by calling the `map` method on the `tokens` variable first.
+                const lineWithTokens = tokens.map((tk, idx) =>
+                  tk.map((t, i) => (
+                    <span
+                      key={i}
+                      {...getTokenProps({ token: t })}
+                      className={i === 0 ? "pl-4" : ""}
+                    />
+                  ))
+                );
+
+                return (
+                  <div key={index} {...getLineProps({ line })}>
+                    <span className="1">{index + 1}</span>
+
+                    {lineWithTokens}
+                  </div>
+                );
+              }
+            )} */}
+
+          {/* CODE SNIPPET HAS MORE THAN 10 LINES */}
+          {tokens.length > MAX_NUMBER_OF_LINES &&
+            tokens.slice(0, MAX_NUMBER_OF_LINES).map((line, index) => (
+              <div key={index} {...getLineProps({ line })}>
+                {/* <span>{index < 9 ? `0${index + 1}` : index + 1}</span> */}
+                <span>{index + 1}</span>
+
+                {line.map((token, idx) => (
+                  <span
+                    key={idx}
+                    {...getTokenProps({ token })}
+                    className={idx === 0 ? "pl-4" : ""}
+                  />
+                ))}
+              </div>
+            ))}
+
+          {/* CODE SNIPPET HAS EXACTLY 10 LINES */}
+          {tokens.length === MAX_NUMBER_OF_LINES &&
+            tokens.map((line, index) => (
+              <div key={index} {...getLineProps({ line })}>
+                {/* <span>{index < 9 ? `0${index + 1}` : index + 1}</span> */}
+                <span>{index + 1}</span>
+
+                {line.map((token, idx) => (
+                  <span
+                    key={idx}
+                    {...getTokenProps({ token })}
+                    className={idx === 0 ? "pl-4" : ""}
+                  />
+                ))}
+              </div>
+            ))}
         </pre>
       )}
     </Highlight>
